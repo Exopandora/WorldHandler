cf3839880aa82f2517fee653d419b3d7fd3b6d4d
Marcel Konrad <mkonrad98@alice-dsl.net>
2018-06-22 00:49:19 +0200

    Renamed EventHandler -> EventListener

--- src/main/java/exopandora/worldhandler/event/EventListener.java
+++ src/main/java/exopandora/worldhandler/event/EventListener.java
@@ -29,7 +29,7 @@ import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 @SideOnly(Side.CLIENT)
-public class EventHandler
+public class EventListener
 {
 	private final FakeCommandHandler commandHandler = new FakeCommandHandler();
 	private final BiomeIndicator biomeIndicator = new BiomeIndicator();
