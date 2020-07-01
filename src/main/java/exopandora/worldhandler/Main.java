fee3d8bc2b7681227511e3d820cf417e6482d093
Marcel Konrad <mkonrad98@alice-dsl.net>
2020-07-02 00:46:11 +0200

    Bump version

--- src/main/java/exopandora/worldhandler/Main.java
+++ src/main/java/exopandora/worldhandler/Main.java
@@ -13,7 +13,7 @@ public class Main
 	public static final String NAME = "World Handler";
 	public static final String MODID = "worldhandler";
 	public static final String MC_VERSION = "1.16.1";
-	public static final String MOD_VERSION = "2.10";
+	public static final String MOD_VERSION = "2.10.1";
 	public static final String URL = "https://minecraft.curseforge.com/projects/world-handler-command-gui";
 	
 	public static void main(String[] args)
